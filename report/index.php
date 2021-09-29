4aafba19629f3ecb3e55c0f6f22bd0358a1339ec
werry216 <linodeBp29@gmail.com>
2021-09-29 17:36:52 +0300

    year-filter

--- report/index.php
+++ report/index.php
@@ -156,6 +156,21 @@
         <div class="row">
           <div class="col col-md-2 col-lg-2 col-xs-12">
             <select class="form-control" id="radiation-year-filter" onchange="radiationChartFilter()">
+              <option value="2000"> 2000 </option>
+              <option value="2001"> 2001 </option>
+              <option value="2002"> 2002 </option>
+              <option value="2003"> 2003 </option>
+              <option value="2004"> 2004 </option>
+              <option value="2005"> 2005 </option>
+              <option value="2006"> 2006 </option>
+              <option value="2007"> 2007 </option>
+              <option value="2008"> 2008 </option>
+              <option value="2009"> 2009 </option>
+              <option value="2010"> 2010 </option>
+              <option value="2011"> 2011 </option>
+              <option value="2012"> 2012 </option>
+              <option value="2013"> 2013 </option>
+              <option value="2014"> 2014 </option>
               <option value="2015"> 2015 </option>
               <option value="2016"> 2016 </option>
               <option value="2017"> 2017 </option>
@@ -245,6 +260,22 @@
         <div class="row">
           <div class="col col-md-2 col-lg-2 col-xs-12">
             <select class="form-control" id="rain-year-filter" onchange="rainChartFilter()">
+              <option value="2000"> 2000 </option>
+              <option value="2001"> 2001 </option>
+              <option value="2002"> 2002 </option>
+              <option value="2003"> 2003 </option>
+              <option value="2004"> 2004 </option>
+              <option value="2005"> 2005 </option>
+              <option value="2006"> 2006 </option>
+              <option value="2007"> 2007 </option>
+              <option value="2008"> 2008 </option>
+              <option value="2009"> 2009 </option>
+              <option value="2010"> 2010 </option>
+              <option value="2011"> 2011 </option>
+              <option value="2012"> 2012 </option>
+              <option value="2013"> 2013 </option>
+              <option value="2014"> 2014 </option>
+              <option value="2015"> 2015 </option>
               <option value="2016"> 2016 </option>
               <option value="2017"> 2017 </option>
               <option value="2018"> 2018 </option>
@@ -260,6 +291,9 @@
               <option value="2028"> 2028 </option>
               <option value="2029"> 2029 </option>
               <option value="2030"> 2030 </option>
+              <option value="2031"> 2031 </option>
+              <option value="2032"> 2032 </option>
+              <option value="2033"> 2033 </option>
             </select>
           </div>
           <div class="col col-md-2 col-lg-2 col-xs-12">
